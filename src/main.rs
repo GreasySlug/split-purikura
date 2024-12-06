217d98476cd490e420b48d7769e5aac1f25b7475
GreasySlug <9619abgoni@gmail.com>
2024-12-06 09:47:35 +0900

    feat: show image

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,62 @@
 use iced::{
+    event::{self, Status},
     font::{Family, Weight},
-    widget::text,
+    widget::{container, image as iced_image, text},
     window::{self, Position},
-    Element, Font, Size, Task, Theme,
+    Element, Event, Font, Length, Size, Subscription, Task, Theme,
 };
+use std::path::PathBuf;
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    InputPath(PathBuf),
+}
 
 #[derive(Debug, Clone)]
-struct State {}
+struct State {
+    input_path: Option<PathBuf>,
+}
 
 impl Default for State {
     fn default() -> Self {
-        Self {}
+        Self { input_path: None }
     }
 }
 
 fn update(state: &mut State, message: Message) -> Task<Message> {
-    match message {}
+    match message {
+        Message::InputPath(path) => {
+            state.input_path = Some(path);
+        }
+    }
+    Task::none()
 }
 
 fn view(state: &State) -> Element<Message> {
-    text("Hello, world!").into()
+    let area = if let Some(input_path) = state.input_path.as_ref() {
+        container(iced_image(input_path))
+            .width(Length::Fill)
+            .height(Length::Fill)
+    } else {
+        container("画像をドラック & ドロップしてください")
+            .style(container::rounded_box)
+            .width(Length::Fill)
+            .height(Length::Fill)
+    };
+
+    area.into()
+}
+
+fn mouse_event_handling(_: &State) -> Subscription<Message> {
+    event::listen_with(|event, status, window| match (event, status, window) {
+        (
+            Event::Window(window::Event::FileHovered(path))
+            | Event::Window(window::Event::FileDropped(path)),
+            Status::Ignored,
+            _,
+        ) => Some(Message::InputPath(path)),
+        _ => None,
+    })
 }
 
 fn set_theme(_: &State) -> Theme {
@@ -47,5 +81,6 @@ fn main() -> iced::Result {
         .default_font(default_font)
         .window(window_settings)
         .theme(set_theme)
+        .subscription(mouse_event_handling)
         .run()
 }
