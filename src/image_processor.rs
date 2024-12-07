ed9b3d0f970210a1262188bd4162b9f23cea731a
GreasySlug <9619abgoni@gmail.com>
2024-12-07 15:51:13 +0900

    feat: config image

--- src/image_processor.rs
+++ src/image_processor.rs
@@ -2,45 +2,97 @@ use std::path::PathBuf;
 
 use image::{imageops::FilterType, GenericImageView, ImageBuffer};
 
+#[derive(Debug, Clone)]
+pub enum PaperSize {
+    A4,
+    A3,
+}
+
+impl PaperSize {
+    pub fn size(&self) -> (f64, f64) {
+        match self {
+            PaperSize::A4 => (210.0, 297.0),
+            PaperSize::A3 => (297.0, 420.0),
+        }
+    }
+
+    pub fn vec_new() -> Vec<Self> {
+        vec![PaperSize::A4, PaperSize::A3]
+    }
+}
+
+impl std::fmt::Display for PaperSize {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PaperSize::A4 => write!(f, "A4"),
+            PaperSize::A3 => write!(f, "A3"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ImageConfig {
+    pub height_mm: f64,
+    pub width_mm: f64,
+    pub diff_mm: f64,
+    pub dpi: f64,
+    pub is_aspect_ratio: bool,
+    pub rows: u32,
+    pub cols: u32,
+}
+
+impl ImageConfig {
+    pub fn size(&self) -> (f64, f64) {
+        (self.width_mm, self.height_mm)
+    }
+}
+
+impl Default for ImageConfig {
+    fn default() -> Self {
+        Self {
+            height_mm: 32.0,
+            width_mm: 22.0,
+            diff_mm: 0.5,
+            dpi: 350.0,
+            is_aspect_ratio: true,
+            rows: 8,
+            cols: 16,
+        }
+    }
+}
+
 const INCH: f64 = 25.4;
 fn convert_mm_to_px(mm: f64, dpi: f64) -> u32 {
     (mm / INCH * dpi).round() as u32
 }
 
 pub fn process_image(
-    height_mm: f64,
-    width_mm: f64,
-    diff_mm: f64,
-    dpi: f64,
-    rows: u32,
-    cols: u32,
-    paper_x: f64,
-    paper_y: f64,
-    is_aspect_ratio: bool,
+    image_config: &ImageConfig,
+    paiser_size: &PaperSize,
     input_path: &PathBuf,
 ) -> Result<ImageBuffer<image::Rgba<u8>, Vec<u8>>, ()> {
-    let width_px = convert_mm_to_px(width_mm, dpi);
-    let height_px = convert_mm_to_px(height_mm, dpi);
-    let diff = convert_mm_to_px(diff_mm, dpi);
-
-    let paper_x_px = convert_mm_to_px(paper_x, dpi);
-    let paper_y_px = convert_mm_to_px(paper_y, dpi);
-
+    let (width_mm, height_mm) = image_config.size();
+    let width_px = convert_mm_to_px(width_mm, image_config.dpi);
+    let height_px = convert_mm_to_px(height_mm, image_config.dpi);
     let Ok(img) = image::open(input_path) else {
         return Err(());
     };
-    let resized_img = if is_aspect_ratio {
+    let resized_img = if image_config.is_aspect_ratio {
         img.resize(width_px, height_px, FilterType::Lanczos3)
     } else {
         img.resize_exact(width_px, height_px, FilterType::Lanczos3)
     };
-    let mut new_img = ImageBuffer::new(paper_y_px, paper_x_px);
-    for y in 0..rows {
-        for x in 0..cols {
+    let (paiser_width_mm, paiser_height_mm) = paiser_size.size();
+    let paiser_width_px = convert_mm_to_px(paiser_width_mm, image_config.dpi);
+    let paiser_height_px = convert_mm_to_px(paiser_height_mm, image_config.dpi);
+    let diff = convert_mm_to_px(image_config.diff_mm, image_config.dpi);
+    let mut new_img = ImageBuffer::new(paiser_height_px, paiser_width_px);
+    for y in 0..image_config.rows {
+        for x in 0..image_config.cols {
             for (i, j, pixel) in resized_img.pixels() {
                 let new_x = x * (width_px + diff) + i;
-                let new_y = y * height_px + j;
-                if new_x < paper_y_px && new_y < paper_x_px {
+                let new_y = y * (height_px + diff) + j;
+                if new_x < height_px && new_y < width_px {
                     new_img.put_pixel(new_x, new_y, pixel);
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,55 @@
 mod image_processor;
+
 use iced::{
     event::{self, Status},
     font::{Family, Weight},
-    widget::{button, column, container, image},
+    widget::{button, column, combo_box, container, image, row, slider, text, toggler},
     window::{self, Position},
-    Element, Event, Font, Length, Size, Subscription, Task, Theme,
+    Alignment, Element, Event, Font, Length, Size, Subscription, Task, Theme,
 };
 use std::path::PathBuf;
 
 #[derive(Debug, Clone)]
 enum Message {
     InputPath(PathBuf),
+    Config(bool),
+    Width(f64),
+    Height(f64),
+    Dpi(f64),
+    Diff(f64),
+    Col(u32),
+    Row(u32),
+    AspectRatio(bool),
+    PaperSize(image_processor::PaperSize),
     Submit,
 }
 
 #[derive(Debug, Clone)]
 struct State {
     input_path: Option<PathBuf>,
+    image_config: image_processor::ImageConfig,
+    paper_size: combo_box::State<image_processor::PaperSize>,
+    selected_paper_size: image_processor::PaperSize,
+    is_config: bool,
 }
 
 impl Default for State {
     fn default() -> Self {
-        Self { input_path: None }
+        Self {
+            input_path: None,
+            image_config: image_processor::ImageConfig::default(),
+            paper_size: combo_box::State::new(image_processor::PaperSize::vec_new()),
+            selected_paper_size: image_processor::PaperSize::A4,
+            is_config: false,
+        }
     }
 }
 
 fn update(state: &mut State, message: Message) -> Task<Message> {
     match message {
+        Message::Config(is_config) => {
+            state.is_config = is_config;
+        }
         Message::InputPath(path) => {
             state.input_path = Some(path);
         }
@@ -34,16 +57,41 @@ fn update(state: &mut State, message: Message) -> Task<Message> {
             let Some(input_path) = state.input_path.as_ref() else {
                 return Task::none();
             };
-
-            let Ok(img) = image_processor::process_image(
-                10.0, 10.0, 0.5, 300.0, 5, 5, 210.0, 297.0, false, input_path,
-            ) else {
+            let image_config = &state.image_config;
+            let selected_paper_size = &state.selected_paper_size;
+            let Ok(img) =
+                image_processor::process_image(image_config, selected_paper_size, input_path)
+            else {
                 return Task::none();
             };
             let output_path = input_path.parent().unwrap().join("output.png");
             img.save(output_path).unwrap();
             state.input_path = None;
             return Task::none();
+        }
+        Message::Width(width) => {
+            state.image_config.width_mm = width;
+        }
+        Message::Height(height) => {
+            state.image_config.height_mm = height;
+        }
+        Message::Dpi(dpi) => {
+            state.image_config.dpi = dpi;
+        }
+        Message::Diff(diff) => {
+            state.image_config.diff_mm = diff;
+        }
+        Message::Col(col) => {
+            state.image_config.cols = col;
+        }
+        Message::Row(row) => {
+            state.image_config.rows = row;
+        }
+        Message::AspectRatio(is_aspect_ratio) => {
+            state.image_config.is_aspect_ratio = is_aspect_ratio;
+        }
+        Message::PaperSize(paper_size) => {
+            state.selected_paper_size = paper_size;
         }
     }
     Task::none()
@@ -61,7 +109,84 @@ fn view(state: &State) -> Element<Message> {
             .height(Length::Fill)
     };
     let submit_button = button("プリを作成").on_press(Message::Submit);
-    column![area, submit_button].into()
+    let is_config = toggler(state.is_config)
+        .label("設定を変更")
+        .on_toggle(Message::Config);
+    let column = column![row![submit_button, is_config], area]
+        .align_x(Alignment::Center)
+        .spacing(10);
+    let column = if state.is_config {
+        column.push(config_view(state))
+    } else {
+        column
+    };
+    column.into()
+}
+
+macro_rules! slider_view {
+    ($label:expr, $min_value:expr, $max_value:expr, $value:expr, $message:expr) => {{
+        let label = text($label).width(Length::Fill);
+        let slider = slider($min_value..=$max_value, $value, $message);
+        let value = text($value.to_string()).width(Length::Fill);
+        row![label, slider, value].spacing(10)
+    }};
+}
+
+fn config_view(state: &State) -> Element<Message> {
+    let dpi = slider_view!(
+        "解像度(DPI)",
+        0.0,
+        1500.0,
+        state.image_config.dpi,
+        Message::Dpi
+    );
+    let width_mm = slider_view!(
+        "幅(mm)",
+        0.0,
+        100.0,
+        state.image_config.width_mm,
+        Message::Width
+    );
+    let height_mm = slider_view!(
+        "高さ(mm)",
+        0.0,
+        100.0,
+        state.image_config.height_mm,
+        Message::Height
+    );
+    let diff_mm = slider_view!(
+        "余白(mm)",
+        0.0,
+        10.0,
+        state.image_config.diff_mm,
+        Message::Diff
+    );
+    let rows = slider_view!("行数", 0, 50, state.image_config.rows, Message::Row);
+    let cols = slider_view!("列数", 0, 50, state.image_config.cols, Message::Col);
+    let paper_size = combo_box(
+        &state.paper_size,
+        "紙のサイズを指定してください",
+        Some(&state.selected_paper_size),
+        Message::PaperSize,
+    )
+    .width(Length::Fill);
+    let aspect_ratio = toggler(state.image_config.is_aspect_ratio)
+        .label("アスペクト比を固定")
+        .on_toggle(Message::AspectRatio);
+    column![
+        dpi,
+        width_mm,
+        height_mm,
+        diff_mm,
+        rows,
+        cols,
+        row![aspect_ratio, paper_size]
+            .align_y(Alignment::Center)
+            .spacing(10)
+    ]
+    .spacing(10)
+    .padding(10)
+    .into()
 }
 
 fn mouse_event_handling(_: &State) -> Subscription<Message> {
@@ -77,7 +202,7 @@ fn mouse_event_handling(_: &State) -> Subscription<Message> {
 }
 
 fn set_theme(_: &State) -> Theme {
-    Theme::Light
+    Theme::CatppuccinFrappe
 }
 
 fn main() -> iced::Result {
